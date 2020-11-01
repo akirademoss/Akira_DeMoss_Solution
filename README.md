f32d650dd2cf6b644e95fbf8d2375a2d7b26722f
akira <akirademoss@gmail.com>
2020-11-01 08:55:13 -0600

    updated documentation

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
-# Instructions for Use on Ubuntu
+# Overview
+The goal of this challenge was to transform the data given in the ```records.log'``` file into a .csv wtih the following columns: ```order_id, weight (lbs), volume (in3)```. 
+
+# Instructions for Use on Linux Ubuntu
 
 ### Prerequisites:
 
-**1.)Create and activate a new virtual envelope**
+**1.) Create and activate a new virtual envelope**
 ```
 python3 -m venv coding_challenge 
 source ./coding_challenge/bin/activate
@@ -18,7 +21,31 @@ sudo apt-get install python3-tk tk-dev
 
 **3.) Run the script and follow the propt to select a file to convert**
 ```
-python solution.py
+python3 solution.py
 ```
+![usage_example1](https://user-images.githubusercontent.com/8731829/97803340-25d90980-1c0f-11eb-8a33-b5b3db070c3d.png)
+
+When running the script, a file browsing GUI where you can select the file you would like to transform will pop up.
 
 **4.) View converted file in same directory as original file**
+
+![usage_example2](https://user-images.githubusercontent.com/8731829/97803348-2b365400-1c0f-11eb-9aec-dd38b8e2c074.png)
+
+Upon successful conversion, the terminal will print the new file's location which will be in the same directory as the original file
+
+### Challenge Reflection
+**Problem Solving**
+The main challenge I faced when writing this script was how to parse the data from the log file in a way that was both simple and intuitive.  
+After recognizing that the log file data was in the dictionary format, I began to look into ways to parse a text dictionary into a python dictionary data structure.  
+I found the simplest way to do this was to use the json library.  My solution opens the file the user wants to transform and reads the file line by line.  In order
+to parse the data using the ```json.loads(')``` method, I simply first replace the ```'``` character with the ```"``` character.  
+
+**Design**
+I wanted to make sure that the script was easy and intuitive to use so I designed it in a way that the user could choose the file they wished to transform using a GUI.  
+Additionaly for ease of use I designed the script to output the transformed file in the same path the original file was located.  In respect to the low-level design, I designed my 
+functions in a way that could easily be added on to to support different types of file conversions.  Finally the column data is stored in a queue which is later popped so that the space complexity is O(1)
+making my solution easily upgradable to handling multiple files at once.
+
+**Summary**
+Overall I really enjoyed re-familiarizing myself with some of the common challenges that occur when working with data for machine learning.  I had a lot of fun with the problem solving
+and design aspects of this challenge and strongly believe that this excercise will help me with projects I complete in the future.
--- solution.py
+++ solution.py
@@ -10,23 +10,24 @@ import csv
 import os
 import tkinter
 import tkinter.filedialog
+from collections import deque 
 
 
-
-order_id, weight, volume = [],[],[]
+#Store column data for file in queue to reduce space complexity
+order_id, weight, volume = deque(),deque(),deque()
 
 #Converts a file to csv format
-def convert_to_csv(): 
-    fpath = get_fpath()
+def transform_to_csv(): 
+    fpath = __get_fpath()
     get_col_data(fpath)
-    fpath = get_new_fpath(fpath, ".csv")
+    fpath = __get_new_fpath(fpath, ".csv")
     #Write columns to .csv file
     with open(fpath, "w") as file:
         csv_file = csv.writer(file)
         csv_file.writerow(["order_id", "weight (lbs)", "volume (in3)"])    
         for i in range(len(order_id)):
-            csv_file.writerow([order_id[i], weight[i], volume[i]])
-    print("conversion complete, file located at: ", fpath)
+            csv_file.writerow([order_id.popleft(), weight.popleft(), volume.popleft()])
+    print("transformation complete, file located at: ", fpath)
 
 #Gets data for .csv format
 def get_col_data(fpath):
@@ -36,10 +37,10 @@ def get_col_data(fpath):
             line = line.replace('\'','\"')
             data = json.loads(line)
             order_id.append(data['order_id'])
-            is_imperial(data['package']['weight'],data['package']['volume'],data['package']['imperial_unit'])
+            __is_imperial(data['package']['weight'],data['package']['volume'],data['package']['imperial_unit'])
             
 #Helper method for get_col_data. If imperial is false, converts units to imperial before appending.  
-def is_imperial(weight_kg, vol_cm3, imperial_unit):
+def __is_imperial(weight_kg, vol_cm3, imperial_unit):
     global order_id, weight, volume
     if imperial_unit == "false":
         converters = {}
@@ -50,11 +51,11 @@ def is_imperial(weight_kg, vol_cm3, imperial_unit):
     weight.append(float(weight_kg))
     volume.append(float(vol_cm3))            
         
-#Helper method that gets the file path of the file you want to convert
-def get_fpath():
+#Helper method that gets the file path of the file you want to transform
+def __get_fpath():
     root = tkinter.Tk()
     while True:
-        print("Please select file to convert")
+        print("Please select a file to transform")
         fpath = tkinter.filedialog.askopenfilename()
         if fpath == None or fpath == "":
             print("You must select a file.")
@@ -64,18 +65,15 @@ def get_fpath():
     return fpath
 
 #Helper method that returns the new filepath based on extension looking to write. e.g. ext = ".csv"
-def get_new_fpath(fpath,file_ext):
+def __get_new_fpath(fpath,file_ext):
     dir_path = os.path.dirname(fpath) 
     fname = os.path.basename(fpath)   
     finfo = os.path.splitext(fname)
     new_fpath = dir_path + "/" + finfo[0] + file_ext
     return new_fpath
 
-#Test method to view the columns
-def print_cols():
-    for i in range(len(order_id)):
-        print(order_id[i], " ", weight[i], " ", volume[i])
-
 if __name__ == '__main__':
-    convert_to_csv()
+    transform_to_csv()
+
     
+
